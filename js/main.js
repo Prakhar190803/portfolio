2f94d80f0b6141e5ea10af3ff594b069d2369f0a
Prakhar190803 <120509130+Prakhar190803@users.noreply.github.com>
2022-12-14 16:45:01 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("cvBtn").addEventListener("click", myFunction);
+document.getElementsByClassName("send").addEventListener("click", myFunction);
+
+
+function myFunction() {
+  alert("");
+}
